32aa4f988c2f989f0775269213ac5bc445288ca4
Ubuntu 18.4 <amir.jamshidijam@gmail.com>
2020-08-28 16:52:30 -0700

    config

--- ccm/config.py
+++ ccm/config.py
@@ -1,11 +1,18 @@
 
 
 
+
+
 class DefaultConfig(object):
 	"""DefaultConfig includes common configurations for flask app.
 	Other config objects inheret these common properties from it.
 	"""
-	pass
+	DB_SERVER = "127.0.0.1"
+	
+	# @property
+	# def DATABASE_URI(self):
+	# 	return 'mysql://user@{}/foo'.format(self.DB_SERVER)
+	
 
 class ProductionConfig(DefaultConfig):
 	pass
