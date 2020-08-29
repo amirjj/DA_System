af8fc0c7081d7699ceb92510df4918efcad9940c
Ubuntu 18.4 <amir.jamshidijam@gmail.com>
2020-08-29 05:26:15 -0700

    init

---
+++ ccm/__init__.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+from config import DevelopmentConfig
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+
+if __name__ == "__main__":
+	print("test")
+	app.run()
+
