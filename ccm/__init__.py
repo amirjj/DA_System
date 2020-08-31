0ec9a0f54cd5ee145b95cd2d44fcd7ac6e47f857
Ubuntu 18.4 <amir.jamshidijam@gmail.com>
2020-08-31 04:50:55 -0700

    cleanup

--- ccm/__init__.py
+++ ccm/__init__.py
@@ -13,6 +13,9 @@ def configure_app(app, test_config):
 def configure_extentions(app):
 	from flask_migrate import Migrate, MigrateCommand
 	from ccm.extentions import db
+	
+	# migrate initiated seperately as it need two parameters 
+	# (in future if needed, it will be add to extentions.py)
 	migrate = Migrate(app, db)
 	from ccm import extentions as ex
 
@@ -39,15 +42,14 @@ def create_app(test_config=None):
 	configure_app(app, test_config)
 	configure_extentions(app)
 	configure_blueprints(app)
-
-
 	#configure_directories
 	#configure_sentry
 
 
 	return app
 
-
+#this function helps if we need to run migration manually(python manage.py db ..)
+#otherwise its irrelevent 
 def migration_helper(test_config=None):
 	from flask import Flask
 	app = Flask(__name__)
@@ -61,10 +63,6 @@ def migration_helper(test_config=None):
 	app.config.from_pyfile('environ.py')
 	from extentions import db
 	db.init_app(app)
-	#configure_directories
-	#configure_sentry
-
-
 	return app, db
 
 
