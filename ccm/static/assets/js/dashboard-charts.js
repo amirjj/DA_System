06bb93c3934a0cfaa6ab66aff3efa0ee1ecd3270
Ubuntu 18.4 <amir.jamshidijam@gmail.com>
2020-09-04 13:47:01 -0700

    adding new controls

--- ccm/static/assets/js/dashboard-charts.js
+++ ccm/static/assets/js/dashboard-charts.js
@@ -1,5 +1,75 @@
 var mss_location_check = document.getElementById("mss_location_check");
-var saleschart = document.getElementById("sales");
+var usn_location_check = document.getElementById("usn_location_check");
+var control_status_radar_this_month = document.getElementById("control_status_radar_this_month");
+var control_status_radar_past_two_months = document.getElementById("control_status_radar_past_two_months");
+
+var control_status_radar_this_month_Chart = new Chart(control_status_radar_this_month,{
+    type: 'radar',
+    data: {
+            labels: ['Possible double selling vouchers','Abnormal Registration','Abnormal COW',
+            'SIMC before activation','Registration outside location','USN Location check',
+            'MSS location check','P2P transfer'],
+            datasets: [{
+                backgroundColor: "rgba(48, 164, 255, 0.5)",
+                borderColor: "rgba(48, 164, 255, 0.8)",
+                data: ['115','116', '115', '114','11','70', '115','100'],
+                label: '',
+                fill: false,
+                showLine: false,
+                pointBorderColor: "rgba(100%, 0%, 0%, 1)",
+                pointBorderWidth: 2,
+                pointHoverBackgroundColor: "rgb(0%, 0%, 0%)",
+                pointHoverRadius: 10,
+            }]
+    },
+    options: {
+        responsive: true,
+        title: {display: false,text: 'Chart'},
+        legend: {position: 'top',display: false,},
+        tooltips: {mode: 'index',intersect: false, enabled: true,},
+        hover: {mode: 'nearest',intersect: true},
+    }
+});
+
+var control_status_radar_past_two_months_Chart = new Chart(control_status_radar_past_two_months,{
+    type: 'radar',
+    data: {
+            labels: ['Possible double selling vouchers','Abnormal Registration','Abnormal COW',
+            'SIMC before activation','Registration outside location','USN Location check',
+            'MSS location check','P2P transfer'],
+            datasets: [{
+                backgroundColor: "rgba(48, 164, 255, 0.5)",
+                borderColor: "rgba(48, 164, 255, 0.8)",
+                data: ['115','116', '115', '114','11','70', '115','100'],
+                label: 'This Month',
+                fill: true,
+                showLine: false,
+                pointBorderColor: "rgba(100%, 0%, 0%, 1)",
+                pointBorderWidth: 2,
+                pointHoverBackgroundColor: "rgb(0%, 0%, 0%)",
+                pointHoverRadius: 10,
+            }, {
+                backgroundColor: "rgba(250, 190, 88, 1)",
+                borderColor: "rgba(250, 190, 88, 1)",
+                data: ['200','100', '20', '114','110','90', '115','120'],
+                label: 'Past Month',
+                fill: true,
+                showLine: false,
+                pointBorderColor: "rgba(100%, 0%, 0%, 1)",
+                pointBorderWidth: 2,
+                pointHoverBackgroundColor: "rgb(0%, 0%, 0%)",
+                pointHoverRadius: 10,
+            }],
+
+    },
+    options: {
+        responsive: true,
+        title: {display: false,text: 'Chart'},
+        legend: {position: 'top',display: false,},
+        tooltips: {mode: 'index',intersect: false, enabled: true,},
+        hover: {mode: 'nearest',intersect: true},
+    }
+});
 
 var MSS_Chart = new Chart(mss_location_check, {
     type: 'line',
@@ -45,37 +115,44 @@ var MSS_Chart = new Chart(mss_location_check, {
     }
 });
 
-var myChart2 = new Chart(saleschart, {
-    type: 'bar',
+var USN_Chart = new Chart(usn_location_check, {
+    type: 'line',
     data: {
-        labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-        datasets: [{
-            label: 'Income',
-            backgroundColor: "rgba(76, 175, 80, 0.5)",
-            borderColor: "#6da252",
-            borderWidth: 1,
-            data: ["280","300","400","600","450","400","500","550","450","650","950","1000"],
-        }]
+            labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','MohammadAmin'
+            ,'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','MohammadAmin'],
+            datasets: [{
+                backgroundColor: "rgba(48, 164, 255, 0.5)",
+                borderColor: "rgba(48, 164, 255, 0.8)",
+                data: ['1135', '1135', '1140','1168', '1150', '1145','1155', '1155', '1150','1160', '1185', '1190'
+                ,'1135', '1135', '1140','1168', '1150', '1145','1155', '1155', '1150','1160', '1185', '1190'],
+                label: '',
+                fill: false,
+                showLine: false,
+                pointBorderColor: "rgba(100%, 0%, 0%, 1)",
+                pointBorderWidth: 2,
+                pointHoverBackgroundColor: "rgb(0%, 0%, 0%)",
+                pointHoverRadius: 10,
+            }]
     },
     options: {
         responsive: true,
         title: {display: false,text: 'Chart'},
         legend: {position: 'top',display: false,},
-        tooltips: {mode: 'index',intersect: false,},
-        hover: {mode: 'nearest',intersect: true},
+        tooltips: {mode: 'index',intersect: false, enabled: true,},
+        hover: {mode: 'nearest',intersect: true},    
         scales: {
             xAxes: [{
                 display: true,
                 scaleLabel: {
-                    display: true,
-                    labelString: 'Months'
+                    display: false,
+                    labelString: 'Name'
                 }
             }],
             yAxes: [{
                 display: true,
                 scaleLabel: {
                     display: true,
-                    labelString: 'Number of Sales'
+                    labelString: 'Count'
                 }
             }]
         }
